d997cef82ec5cbd01d4897d39a989cc07950c1cd
srishtigarg1620 <104136574+srishtigarg1620@users.noreply.github.com>
2023-05-26 18:37:09 +0530

    Add files via upload

---
+++ movie_file.py
@@ -0,0 +1,9 @@
+
+import os
+import shutil
+
+from_dir = "C:\\Users\\srishti\\Downloads"
+to_dir = "C:\\Users\\srishti\\Desktop\\srishti coder\\clases\\document_files"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
